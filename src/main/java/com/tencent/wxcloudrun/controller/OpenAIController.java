32188416a160a4a666403160af67902d75dcab33
w5l6j9 <381414439@qq.com>
2023-10-25 11:21:34 +0800

    fix:bug fix

--- src/main/java/com/tencent/wxcloudrun/controller/OpenAIController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/OpenAIController.java
@@ -7,8 +7,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@RestController
 public class OpenAIController {
 
     @Value("${openai.api.key}")
